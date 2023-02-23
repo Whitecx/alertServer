ae1b02a5e2595c52eb5e7698e5a345e323c182fc
Christian White <christianwhite@Christian-Whites-MacBook-Pro.local>
2023-02-23 10:34:34 -0500

    massive commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as MailService from 'sendEmail.js';
+import * as MailService from './sendEmail.js';
 
 const app = express();
 app.use(express.json());
@@ -7,23 +7,28 @@ app.listen(9090);
 
 const RECIPIENT_LIST = process.env.RECIPIENT_LIST;
 
-app.post((req, res)=>{
+app.post("/", (req, res)=>{
 	console.log("Request received");
+	console.log("Recipients: " + RECIPIENT_LIST);
 	//parse input
 	let body = req.body;
 	let subject = body.subject ? body.subject : "";
 	let text = body.text ? body.text : "";
 
 	//send error if to addresses aren't configured
-	if(!RECIPIENT_LIST){ res.status(400).send("Error: Please configure list of recipients in .env file");}
+	if(!RECIPIENT_LIST){ return res.status(400).send("Error: Please configure list of recipients in .env file");}
 
 	//parse .env list of recipient addresses
 	let recipients = parseRecipients(RECIPIENT_LIST);
 
 	//send email to all recipients
-	sendAll(recipients, subject, text);
-	//TODO Convert to promiseAll to return success/fail on email sends
-	res.status(200).send("ok");
+	let promises = sendAll(recipients, subject, text);
+	//Return ok if all promises resolve
+	Promise.all(promises)
+	.then(values => {
+		return res.status(200).send("ok");
+		//Return 500 if one of the promises is rejected
+	}).catch((err)=>{ return res.status(500).send("Check text Server logs");});
 });
 
 const parseRecipients = (list) => {
@@ -31,8 +36,12 @@ const parseRecipients = (list) => {
 	return list.split(',');
 }
 
+//Sends an email to each recipient. Returns an array of promises for each email send
 const sendAll = (recipients, subject, text) => {
+	let promises = [];
 	recipients.forEach(address => {
-		MailService.sendMail(address, subject, text);
+		let promise = MailService.sendMail(address, subject, text);
+		promises.push(promise);
 	});
+	return promises;
 }
